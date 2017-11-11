837bcd4a72293a22de06d1ddb9262ab6a93ad112
Sergey <sunliveua@gmail.com>
2017-11-11 14:46:30 +0200

    0.1.7

---
+++ src/components/form.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="formContainer">
+    <form class="form">
+      <h2>Оставьте свои данные</h2>
+      <div class="wrap">
+        <img src="../assets/clock.svg" alt="clock">
+        <span>Затратите 19 секунд</span>
+      </div>
+
+      <div class="field">
+        <label class="label">Имя</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="name">
+        </div>
+      </div>
+    
+      <div class="field">
+        <label class="label">Телефон</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="phone">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Город</label>
+        <div class="control">
+          <div class="select">
+            <select>
+              <option>Select dropdown</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <p>Наш менеджер уже набирает <br> ваш номер</p>
+      <div class="myButton">Отправить</div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userData: {
+        name: '',
+        phone: '',
+        city: '',
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.formContainer {
+  min-height: 600px;
+  background-image: radial-gradient(circle at 31% 18%, #2894e6, #157bc8);
+  background-color: var(--mainBlue);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.form {
+  width: 430px;
+  height: 530px;
+  background-color: #fff;
+  border-radius: 11px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  & p {
+    text-align: center;
+    margin: 1rem 0;
+    color: var(--mainBlue);
+    opacity: .5;
+    font-family: Helvetica, Arial, serif;
+  }
+  & .myButton {
+    min-width: 130px;
+  }
+  & h2 {
+    font-size: responsive 1.5rem 2rem;
+    color: var(--mainBlue);
+  }
+}
+.wrap {
+  display: flex;
+  align-items: center;
+  color: var(--mainBlue);
+  font-family: Helvetica, Arial, serif;
+  font-style: oblique;
+}
+.select select {
+  width: 185px;
+}
+</style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -4,6 +4,7 @@
     <app-howitswork></app-howitswork>
     <app-heal></app-heal>
     <app-reviews></app-reviews>
+    <app-form></app-form>
   </main>
 </template>
 
@@ -15,6 +16,7 @@ import why from './why';
 import howitswork from './howitswork';
 import heal from './heal';
 import reviews from './reviews';
+import form from './form';
 
 export default {
   components: {
@@ -22,6 +24,7 @@ export default {
     'app-howitswork': howitswork,
     'app-heal': heal,
     'app-reviews': reviews,
+    'app-form': form,
   },
 };
 </script>
