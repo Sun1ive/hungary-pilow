66169ae283d28b10b3438d24ae5fe257610e28fc
Sergey <sunliveua@gmail.com>
2017-11-11 11:35:54 +0200

    0.1.6

--- src/components/main.vue
+++ src/components/main.vue
@@ -3,6 +3,7 @@
     <app-why></app-why>
     <app-howitswork></app-howitswork>
     <app-heal></app-heal>
+    <app-reviews></app-reviews>
   </main>
 </template>
 
@@ -13,12 +14,14 @@
 import why from './why';
 import howitswork from './howitswork';
 import heal from './heal';
+import reviews from './reviews';
 
 export default {
   components: {
     'app-why': why,
     'app-howitswork': howitswork,
     'app-heal': heal,
+    'app-reviews': reviews,
   },
 };
 </script>
---
+++ src/components/reviews.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="reviews">
+    <div class="wrapper">
+      <h2>Отзывы</h2>
+      <p>Спасибо за отзывы, вы нас очень мотивируете на дальнейшую работу</p>
+    </div>
+    <slider 
+    animation="normal"
+    width="900px">
+      <slider-item v-for="(item, i) in items" :key="i">
+        <div>{{ item }}</div>
+      </slider-item>
+    </slider>
+
+    <div class="arrow"></div>
+    <div class="myButton">Заказать услугу</div>
+  </div>
+</template>
+
+<script>
+import { Slider, SliderItem } from 'vue-easy-slider';
+
+export default {
+  data() {
+    return {
+      items: ['hello world', 'hello there', 'thanks'],
+    };
+  },
+  components: {
+    Slider,
+    SliderItem,
+  },
+};
+</script>
+
+<style scoped>
+@import '../css/slider.css';
+
+.reviews {
+  background-color: #c0c0c0;
+  min-height: 600px;
+  color: #fff;
+  position: relative;
+  & .wrapper {
+    text-align: center;
+    & h2 {
+      font-size: responsive 2rem 4rem;
+      padding-top: 3rem;
+    }
+    & p {
+      font-family: CharterC, Arial, Helvetica, sans-serif;
+      width: 40%;
+      margin: 0 auto;
+    }
+  }
+  & .myButton {
+    position: absolute;
+    bottom: 5%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  & .arrow {
+    bottom: 10%;
+    left: 50%;
+  }
+}
+</style>
