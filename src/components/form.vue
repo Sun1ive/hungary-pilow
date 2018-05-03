d98bf253995b18b83372206b750182dee1625757
Sun1ive <sunliveua@gmail.com>
2018-05-03 10:19:58 +0300

    123

--- src/components/form.vue
+++ src/components/form.vue
@@ -50,16 +50,15 @@ export default {
   methods: {
     submit() {
       Email.send(
-        `coats@indresser.com`,
-        // 'info@indresser.com',
-        'vi290983mal@gmail.com',
+        `info@parnatisztitas.com`,
+        'vi290983mal@gmail.com, cudlencovadim@gmail.com',
         'Заказ с сайта Párna Tisztítás',
         `Пользователь: ${this.userData.name},
         Город: ${this.userData.city},
         Телефон: ${this.userData.phone}`,
         'mail.adm.tools',
-        'coats@indresser.com',
-        '3DLao3x1AC8t',
+        'info@parnatisztitas.com',
+        'i6d85XUavAT6',
       );
       // this.$emit('closeOrder');
       this.isActive = true;
